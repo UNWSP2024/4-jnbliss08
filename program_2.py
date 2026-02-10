10067734c855fb0a780523c88def87dc9b2273cf
jnbliss08 <jnbliss08@gmail.com>
2026-02-10 19:39:18 +0000

    commit

--- program_2.py
+++ program_2.py
@@ -1,14 +1,14 @@
 # Program #2: Movie Tix
-# Write a program that has the user input various movie names and how many 
-# tickets are desired for each movie.  
-# At the end of the program it prints out the total number of tickets desired by the user.  
+# Write a program that has the user input various movie names and how many
+# tickets are desired for each movie.
+# At the end of the program it prints out the total number of tickets desired by the user.
 # Use either a "for loop" or "while loop" to accomplish this.
+total_tickets = 0
+movies = ['Cars', 'Tron', 'Star Wars', 'Lord of the Rings', 'Black Hawk Down']
+for movie in movies:
+    tickets = int(input(f'how many tickets do you want for {movie}? '))
+    while tickets < 0: 
+        tickets = int(input('Error please enter a positive number: '))    
+    total_tickets += tickets
 
-def main():
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
-
-
-if __name__ == '__main__':
-    main()
+print(f'you want {total_tickets} tickets')
